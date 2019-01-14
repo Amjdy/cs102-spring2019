00c403142c7aaa6ef38cbfb46b7392d76e0dcd66
aniskoubaa <akoubaa@coins-lab.org>
2019-01-14 09:50:34 +0300

    fall2017

--- cs102-spring2019/src/topic1/classes/Student.java
+++ cs102-spring2019/src/topic1/classes/Student.java
@@ -3,9 +3,10 @@ package topic1.classes;
 public class Student {
     
     //attributes: properties
-    String firstName;
-    String lastName;
-    int age;   
+    private String firstName;
+    private String lastName;
+    private int age;   
+    private String mobilePhone;
     
     
     //constructor: it allows to assign values to attributes at creation time
@@ -13,10 +14,15 @@ public class Student {
     //2= it must be public 
     //3- it does not return anything
     
-    public Student(String fn, String ln, int a){
-        firstName=fn;
-        lastName = ln;
-        age = a;
+    public Student(String fn, String ln, int a,String mp){
+        //firstName=fn;
+        setFirstName(fn);
+        //lastName = ln;
+        setLastName(ln);
+        //age = a;
+        setAge(a);
+        //mobilePhone=mp;
+        setMobilePhone(mp);
     }
     
     
@@ -29,14 +35,52 @@ public class Student {
     //operations: methods
     public void printInfo(){
         System.out.println("Student Info");
-        System.out.printf("Student %s %s is %d years old\n",
-                firstName, lastName, age);
+        System.out.printf("Student %s %s is %d years old. his mobile phone is %s\n",
+                firstName, lastName, age, mobilePhone);
     }
     
+    
+    //to read the value: get
+    public String getFirstName(){
+        return firstName;
+    }
+    
+    //to change the value: set
+    public void setFirstName(String firstName){
+       this.firstName = firstName;
+    }
+    
+    public String getLastName(){
+        return lastName;
+    }
+    
+    public void setLastName(String ln){
+        this.lastName = ln;
+    }
     
     public int getAge(){
         return age;
     }
+    
+    public void setAge(int a){
+        if (a>0)
+            age = a;
+        else
+            throw new IllegalArgumentException("age must be positive");
+    }
+    
+    public String getMobilePhone(){
+        return mobilePhone;
+    }
+    
+    public void setMobilePhone(String mp){
+        //must start with 05 and it has 10 digit
+        if (mp.startsWith("05") &&(mp.length()==10))
+            mobilePhone = mp;
+        else
+            throw new IllegalArgumentException("wrong phone format");
+    }
+    
     
     
 }
--- cs102-spring2019/src/topic1/classes/StudentTest.java
+++ cs102-spring2019/src/topic1/classes/StudentTest.java
@@ -7,16 +7,27 @@ public class StudentTest {
      public static void main(String []args){
     
          //create my three students
-         Student student1 = new Student("Mohamed", "Ali",23);//the constructor  
-         Student student2 = new Student("Mounir","Kamal",25);//the DEFAULT constructor
+         Student student1 = new Student("Mohamed", "Ali",23,"0589873484");//the constructor  
+         Student student2 = new Student("Mounir","Kamal",25,"0584345484");//the DEFAULT constructor
  
-         Student student3 = new Student("Sami","Adel",27);//the constructor
+         Student student3 = new Student("Sami","Adel",27,"0584345489");//the constructor
+         
+         
+         //student1.firstName="Salah";
+         student1.setFirstName("Salah");
+         //student2.age = -33;//data is not valid 
+         //student2.setAge(33);
+         
+         
+         System.out.println("student1 first name: "+ student1.getFirstName());
          
          student1.printInfo();
          student2.printInfo();
-         student2.printInfo();
+         student3.printInfo();
          
-         Student student4 = new Student("Samir", "Waleed", 30);//the constructor
+         
+         
+         Student student4 = new Student("Samir", "Waleed", 30,"0598393827");//the constructor
          student4.printInfo();
 
      }
